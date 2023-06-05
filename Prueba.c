2635d07e4fa402d57f3ddadd0f7af5b3ac091092
UdlaAriel <sebastian.ramirez@udla.edu.ec>
2023-06-05 16:21:39 -0500

    Cuarta versión

--- Prueba.c
+++ Prueba.c
@@ -2,42 +2,60 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define alumnos 23
+#define alumnos 24
 #define progresos 4
 
-void imprimir_Matriz(int calificaciones[alumnos][progresos])
+void promedio_general(int calificaciones[alumnos][progresos])
 {
-    for (int i = 0; i < alumnos; i++){
-        for (int j = 0; j < progresos; i++){
-            printf(" %d", calificaciones[i][j]);
-        }
-        printf("\n");
+    float sumTotal;
+    float promedio;
+
+    for (int i = 0; i < alumnos; i++)
+    {
+        sumTotal += calificaciones[i][4];
     }
+    
+    promedio = sumTotal/alumnos;
+    printf("\nEl PROMEDIO GENERAL fue: ");
+    printf("%d\n");
 }
 
-int promedio(int calificaciones[alumnos][progresos])
+void mayor_promedio(int calificaciones[alumnos][progresos])
 {
-    int promedio_final;
-    
 
-    return promedio_final;
 }
 
 int main()
 {
-    int calificaciones[alumnos][progresos];
-    int notaFinal;
+    int calificaciones[alumnos][progresos] = {0};
+    float sumTotal[alumnos] = {0}; 
+    float promedio_individual[alumnos] = {0};
 
     srand(time(NULL));
 
     /*Asignación de valores randómicos al arreglo calificaciones*/
     for (int i = 0; i < alumnos; i++)
     {
-        for (int j = 0; j < progresos; i++){
+        printf("Estudiante %d: ", i);
+        
+        for (int j = 0; j < progresos; j++){
 
             calificaciones[i][j] = rand() % 11;
+            sumTotal[i] += calificaciones[i][j];
+            if(j == (progresos -1))
+            {
+                promedio_individual[i] = sumTotal[i]/progresos;
+                calificaciones[i][j] = promedio_individual[i];
+            }
+            printf(" %d", calificaciones[i][j]);
         }
+
+        printf(" \n");
     }
+
+    promedio_general(calificaciones);
+
+    mayor_promedio(calificaciones);
 
     return 0;
 }
